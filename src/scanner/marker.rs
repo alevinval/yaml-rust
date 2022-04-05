6cc7386c71cd44641a583e7e083085247d902fec
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-04-05 22:42:39 +0200

    scanner: clean-up unused methods

--- src/scanner/marker.rs
+++ src/scanner/marker.rs
@@ -9,16 +9,4 @@ impl Marker {
     pub fn new(index: usize, line: usize, col: usize) -> Marker {
         Marker { index, line, col }
     }
-
-    pub fn index(&self) -> usize {
-        self.index
-    }
-
-    pub fn line(&self) -> usize {
-        self.line
-    }
-
-    pub fn col(&self) -> usize {
-        self.col
-    }
 }
